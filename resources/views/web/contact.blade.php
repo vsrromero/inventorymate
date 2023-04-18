5ccbdaadc7014926252b6015c2a792a598c0a870
Victor Romero <vsr.romero@gmail.com>
2023-04-18 17:32:27 +0100

    added main routes

---
+++ resources/views/web/contact.blade.php
@@ -0,0 +1,15 @@
+<h1>Contact</h1>
+<ul>
+    <li>
+        <a href=" {{ route('web.home') }} ">Home</a>
+    </li>
+    <li>
+        <a href=" {{ route('web.about') }} ">About</a>
+    </li>
+    <li>
+        <a href=" {{ route('web.contact') }} ">Contact</a>
+    </li>
+    <li>
+        <a href=" {{ route('web.login') }} ">Login</a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'home'])->name('web.home');
+Route::get('/about', [AboutController::class, 'about'])->name('web.about');
+Route::get('/contact', [ContactController::class, 'contact'])->name('web.contact');
+Route::get('/login', function(){return 'login';})->name('web.login');
 
-Route::get('/', function () {
-    return view('welcome');
+// restricted area
+Route::prefix('app')->group(function(){
+    Route::get('/clients', function(){return 'clients';})->name('app.clients');
+    Route::get('suppliers', function(){return 'suppliers';})->name('app.suppliers');
+    Route::get('products', function(){return 'products';})->name('app.products');
 });
