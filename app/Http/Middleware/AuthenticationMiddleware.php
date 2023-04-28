fc5977509943788505278362d50b26362582618b
Victor Romero <vsr.romero@gmail.com>
2023-04-28 21:40:50 +0100

    add repository to get all suppliers from db

---
+++ app/Http/Middleware/AuthenticationMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\Request;
+
+class AuthenticationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //return $next($request);
+        return Response('Unauthorized', 401);
+    }
+}
--- resources/views/app/supplier/index.blade.php
+++ resources/views/app/supplier/index.blade.php
@@ -2,9 +2,12 @@
 
 {{-- comentário do blade --}}
 
-@for($i = 0; $i < 10; $i++)
-    <p>Supplier: {{ $supplierNames[$i] }}  </p>
-@endfor
-<h2>DD Blade</h2>
-@dd($supplierNames)
-<h2>var_dump PHP</h2>
+@foreach($supplierNames as $supplier)
+    <div>
+        Supplier: {{ $supplier['name'] }}<br />  
+        Email: {{ $supplier['email'] }}<br /> 
+        Phone: {{ $supplier['phone'] }}<br /> 
+    </div>
+    <br />
+@endforeach
+
