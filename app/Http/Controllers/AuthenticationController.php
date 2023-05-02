fad7c74ad9f5d57838ad6fd7b870aa9a20284835
Victor Romero <vsr.romero@gmail.com>
2023-05-02 02:18:44 +0100

    Changed the login method to use bcrypt

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
@@ -22,40 +23,33 @@ class AuthenticationController extends Controller
             'user' => 'email',
             'password' => 'required'
         ];
-
+    
         // Message to be displayed if validation fails
         $feedback = [
             'user.email' => 'Please enter a valid email address',
             'password.required' => 'Please enter your password'
         ];
-
+    
         $request->validate($authentication, $feedback);
-
+    
         // check on db if user exists
         $email = $request->get('user');
         $password = $request->get('password');
-
-        $user = new User();
-
-        $user = $user->where('email', $email)
-                        ->where('password', $password)
-                        ->get()
-                        ->first();
-
-        if(isset($user->name)) {
-
+    
+        $user = User::where('email', $email)->first();
+    
+        if ($user && Hash::check($password, $user->password)) {
             session_start();
-            global $_SESSION;
             $_SESSION['name'] = $user->name;
             $_SESSION['email'] = $user->email;
-
+    
             return redirect()->route('app.home');
         } else {
-            // User does not exist
+            // User does not exist or password is incorrect
             return redirect()->route('web.login')->with('error', 'Invalid email or password');
         }
-
     }
+    
 
     public function logout()
     {
